5aa546e472812535c4da1577334e3592653484bd
Aredhele <vincentstehly@hotmail.fr>
2016-12-10 11:30:11 +0100

    Add Menu State

---
+++ Source/GameState/MenuState.cpp
@@ -0,0 +1,33 @@
+#include "GameState/MenuState.hpp"
+
+/* explicit */ MenuState::MenuState()
+{
+    // None
+}
+
+void MenuState::init()
+{
+
+}
+
+/* virtual */ MenuState::~MenuState()
+{
+    // None
+}
+
+/* virtual */ void MenuState::update(double dt)
+{
+    // TODO
+}
+
+bool MenuState::onEnter()
+{
+    // TODO
+    return true;
+}
+
+bool MenuState::onExit()
+{
+    // TODO
+    return true;
+}
