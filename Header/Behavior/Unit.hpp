682497d8506fbb411a01280c5d5cdf3fb171874b
Renaud Belloncle <renaud-belloncle@orange.fr>
2016-12-10 11:15:42 +0100

    Unit - Position

--- Header/Behavior/Unit.hpp
+++ Header/Behavior/Unit.hpp
@@ -1,12 +1,24 @@
 #ifndef __UNIT_HPP
 #define __UNIT_HPP
 
+#include <SFML/System.hpp>
+
 class Unit
 {
 public:
-    virtual void targeted();
-private:
-    int life;
+    Unit(sf::Vector2f const& startPosition, int life)
+        : m_position(startPosition),
+          m_life(life)
+    {
+        // nothing
+    }
+
+    virtual void targeted(int damage) = 0;
+    virtual sf::Vector2f getPosition() const = 0;
+
+protected:
+    int m_life;
+    sf::Vector2f m_position;
 };
 
 #endif // __UNIT_HPP
