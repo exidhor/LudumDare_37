386b8d17e8337885245b8ba1316274f7c1a3ae47
Jipay <jipay.detiege@gmail.com>
2016-12-10 12:06:43 +0100

    deplacement

--- Header/MathHelper.hpp
+++ Header/MathHelper.hpp
@@ -1,11 +1,24 @@
 #pragma once
-#include <SFML/System.hpp>
+
+#include <cstdint>
+#include <iostream>
+#include <math.h>
+#include <SFML/Graphics.hpp>
 
 namespace MathHelper
 {
+	sf::Vector2f abs(sf::Vector2f const& vector);
+}
+
+namespace Movement
+{
+	sf::Vector2f offset(sf::Vector2f const& start, sf::Vector2f const& end);
+
+	float distance(sf::Vector2f const& start, sf::Vector2f const& end);
+
 	/**
-	 * return the deplacement of an object (the vector2f returned has to
-	 * be add to the position)
-	 */
-	sf::Vector2f moveEntity(sf::Vector2f const& direction, float speed);
+	* return the deplacement of an object (the vector2f returned has to
+	* be add to the position)
+	*/
+	sf::Vector2f deplacement(sf::Vector2f const& start, sf::Vector2f const& direction, double speed);
 }
--- Source/MathHelper.cpp
+++ Source/MathHelper.cpp
@@ -1,6 +1,38 @@
 #include "MathHelper.hpp"
 
-sf::Vector2f MathHelper::moveEntity(sf::Vector2f const& direction, float speed)
+sf::Vector2f MathHelper::abs(sf::Vector2f const& vector)
 {
-	
+	return sf::Vector2f(fabsf(vector.x), fabsf(vector.y));
+}
+
+sf::Vector2f Movement::offset(sf::Vector2f const& start, sf::Vector2f const& end)
+{
+	return end - start;
+}
+
+float Movement::distance(sf::Vector2f const& start, sf::Vector2f const& end)
+{
+	sf::Vector2f distances = MathHelper::abs(offset(start, end));
+
+	return std::sqrt(distances.x * distances.x + distances.y * distances.y);
+}
+
+sf::Vector2f Movement::deplacement(sf::Vector2f const& start, sf::Vector2f const& direction, double speed)
+{
+	if (speed < 0)
+	{
+		std::cerr << "Speed < 0 during a call to deplacement" << std::endl;
+		return sf::Vector2f(); // no deplacement;
+	}
+
+	float distanceValue = distance(start, direction);
+
+	if (distanceValue <= speed)
+	{
+		return offset(start, direction);
+	}
+
+	double ratio = speed / distanceValue;
+
+	return offset(start, direction) * (float)ratio;
 }
