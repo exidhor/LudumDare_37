bd5e8f7667e238e8e14f2101e9aa188681131f28
Renaud Belloncle <renaud-belloncle@orange.fr>
2016-12-10 15:29:52 +0100

    Add Fly sprites

--- Source/DemoniacObject/Fly.cpp
+++ Source/DemoniacObject/Fly.cpp
@@ -2,11 +2,27 @@
 // Created by renaud on 10/12/16.
 //
 
+#include <Memory/Container.hpp>
+#include "Memory/PoolAllocator.hpp"
 #include "DemoniacObject/Fly.hpp"
-#include "Projectile.hpp"
 
 Fly::Fly(sf::Vector2f const &startPosition)
     : DemoniacObject(startPosition, FLY_LIFE, FLY_SPEED, FLY_DAMAGE, FLY_ARMOR)
 {
-    // nothing
+    sf::Sprite *sprite = PoolAllocator<sf::Sprite>::Instance()->Allocate();
+    sprite->setTexture(*Container<sf::Texture>::Instance()->GetResource("FLY_1"));
+    addSprite(sprite);
+    PoolAllocator<sf::Sprite>::Instance()->Deallocate(sprite);
+
+    sprite = PoolAllocator<sf::Sprite>::Instance()->Allocate();
+    sprite->setTexture(*Container<sf::Texture>::Instance()->GetResource("FLY_2"));
+    addSprite(sprite);
+    PoolAllocator<sf::Sprite>::Instance()->Deallocate(sprite);
+
+    sprite = PoolAllocator<sf::Sprite>::Instance()->Allocate();
+    sprite->setTexture(*Container<sf::Texture>::Instance()->GetResource("FLY_3"));
+    setDeathSprite(sprite);
+    PoolAllocator<sf::Sprite>::Instance()->Deallocate(sprite);
+
+    nextSprite();
 }
